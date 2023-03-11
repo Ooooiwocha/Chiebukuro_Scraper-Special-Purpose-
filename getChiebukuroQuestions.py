7692ff4f80f70846942b8436c7f2990adc19fed7
Ooooiwocha <77987608+Ooooiwocha@users.noreply.github.com>
2023-03-11 17:31:23 +0900

    Update getChiebukuroQuestions.py

--- getChiebukuroQuestions.py
+++ getChiebukuroQuestions.py
@@ -5,6 +5,7 @@ import pandas as pd
 from time import sleep
 import logging
 import os
+import datetime
 
 BASE_URL = "https://chiebukuro.yahoo.co.jp/search"
 
@@ -66,10 +67,24 @@ class WorkSheet:
 		return self.myworksheet.col_values(i);
 
 class URLBuilder:
+
 	class Chiebukuro_Params(dict):
+		DEFAULT_FLAG = dict(flg="1", dflg="4");
+		DEFAULT_SINCE = dict(dfrom_y="2015", dfrom_m="04", dfrom_d="01");
+		DEFAULT_UNTIL = dict(dto_y="2021", dto_m="12", dto_d="01");
+		def __init__(self, params=dict(), flag=DEFAULT_FLAG, since=DEFAULT_SINCE, until=DEFAULT_UNTIL):
+			params.update(flag);
+			params.update(since);
+			params.update(until);
+			return super().__init__(params);
+
 		def set_next_page(self):
 			next_page = int(self["b"]) + 10;
 			self["b"] = str(next_page);
+
+		def set_next_year(self):
+			next_year = int(self["dfrom_y"]) + 1;
+			self["dfrom_y"] = str(next_year);
 
 	url = "";
 	params = Chiebukuro_Params();
@@ -84,6 +99,11 @@ class URLBuilder:
 
 
 class Scraper:
+	base_url = BASE_URL;
+	DEC = "20";
+	ASC = "21";
+	PAGE_MAX = 100;
+	DBG_UPDATE_PARAMS = 2;
 	class History(set):
 		def __init__(self):
 			os.makedirs("data", exist_ok=True);
@@ -108,18 +128,45 @@ class Scraper:
 			column_no = self.COLUMN_NO;
 			super().__init__(ws.col_values(column_no)[1:]);
 
-	search_text = "hoge";
-	base_url = BASE_URL;
-
 	def __init__(self, text: str):
 		self.search_text = text;
 		self.driver = webdriver.Chrome();
 
-	def scrape_execute(self, params: dict, ws: WorkSheet, url_keys: set, userid_keys: set, visited: set, dbg=False) -> None: #参照渡し
+	def is_chronical_order(self, params:dict) -> bool:
+
+		if params["sort"] == self.DEC or params["sort"] == self.ASC:
+			return True;
+		return False;
+
+	def update_params(self, params:dict, date:str):
+		MIN_DELTA = 90;
+		y, m, d = date.split('/');
+		d = d.split(" ")[0];
+		date1 = None;
+		date2 = datetime.date(*map(int, [y, m, d]));
+		if params["sort"]==self.ASC:
+			date1 = datetime.date(*map(int, [params["dfrom_y"], params["dfrom_m"], params["dfrom_d"]]));
+			delta = date2 - date1;
+			if delta.days<MIN_DELTA: ##知恵袋の仕様上、日付検索が意図的に弱められている可能性 -> 日付後退現象を防ぐ必要がある
+				date3 = date1 + datetime.timedelta(days=90);
+				y, m, d = map(str, [date3.year, date3.month, date3.day]);
+
+			params.update({"b": "1", "dfrom_y": y, "dfrom_m": m, "dfrom_d": d});
+
+		elif params["sort"]==self.DEC:
+			date1 = datetime.date(*map(int, [params["dto_y"], params["dto_m"], params["dto_d"]]));
+			delta = date1 - date2;
+			if delta.days<MIN_DELTA:
+				date3 = date1 + datetime.timedelta(days=-90);
+				y, m, d = map(str, [date3.year, date3.month, date3.day]); 
+
+			params.update({"b": "1", "dto_y": y, "dto_m": m, "dto_d": d});
+
+	def scrape_execute(self, params: dict, ws: WorkSheet, url_keys: set, userid_keys: set, visited: set, dbg=0) -> None: #参照渡し
 		page = 0;
 		
 		## 検索結果がなくなるまで取得
-		while page < 100:
+		while page < self.PAGE_MAX:
 			url = URLBuilder(self.base_url, params).build();
 			logger.info("URLBuild Success: {}".format(url));
 			self.driver.get(url);
@@ -141,18 +188,22 @@ class Scraper:
 				q = href.split('/')[-1];
 				if q in visited:
 					if dbg: print("already checked!");
+					sleep(0.1);
 					continue;
 				else:
 					visited.add(q);
+					sleep(0.1);
 					with open("data/checked.txt", mode='a', encoding="utf-8", newline="\n") as f:
 						f.write(q+"\n");
-
+				sleep(0.1);
 				text_content = a_elem.text;
 
 				self.driver.execute_script("window.open()");
 				sleep(1);
 				self.driver.switch_to.window(self.driver.window_handles[1]);
+				sleep(0.1);
 				self.driver.get(href);
+				sleep(0.1);
 				area_questioner = None;
 				
 				try:
@@ -166,15 +217,15 @@ class Scraper:
 
 				question_date = area_questioner.find_element(By.CSS_SELECTOR, "p[class^=ClapLv1UserInfo_Chie-UserInfo__Date]").text;
 				user_name = area_questioner.find_element(By.CSS_SELECTOR, "p[class^=ClapLv1UserInfo_Chie-UserInfo__UserName]").text[:-2];
-
 				user_page_url = area_questioner.get_attribute("href");
-
 				self.driver.get(user_page_url);
+				sleep(0.1);
 				profile = self.driver.find_element(By.ID, "my_prof");
 				user_id = self.driver.find_element(By.CSS_SELECTOR, "p[class^=ClapLv2MyProfile_Chie-MyProfile__Uid]").text.split("：")[-1];
 				record = [question_date, href, text_content, "", user_name, user_id];
 				
 				self.driver.execute_script("window.close()");
+				sleep(0.1);
 				self.driver.switch_to.window(self.driver.window_handles[0]);
 				sleep(1);
 
@@ -187,28 +238,34 @@ class Scraper:
 					ws.myworksheet.format("A{}".format(len(ws.myworksheet.col_values(1))), {"numberFormat": {"type": "DATE"}})
 					url_keys.add(record[1]);
 					userid_keys.add(record[-1]);
+					sleep(0.5);
 
 			## ページング処理
 			params.set_next_page();
-
+			if dbg==self.DBG_UPDATE_PARAMS: page = self.PAGE_MAX;
+			last_question_date = self.driver.find_elements(By.CSS_SELECTOR, "span[class^=ListSearchResults_listSearchResults__informationDate]")[-1].text.replace("\n", "").split('：')[1];
+		
+		## 時系列ソートの場合ににおいては、検索対象期間をずらして特別な処理を行う
+		if page == self.PAGE_MAX and self.is_chronical_order(params):
+			self.update_params(params, last_question_date);
+			logger.info("page 100 reached: new params = {}".format(params));
+			self.scrape_execute(params, ws, url_keys, userid_keys, visited, dbg);
 
 def main():	
 	visited = Scraper.History();
-	spreadsheet_key = input("INPUT SPREADSHEET KEY");
+	spreadsheet_key = input("INPUT SPREADSHEET KEY" + "\n");
 	ss = SpreadSheet(spreadsheet_key);
 	ws = WorkSheet(0, ss);
 	url_keys = Scraper.URL_Set(ws);
 	userid_keys = Scraper.Nomenclature(ws);
 	while True:
 		ws.myworksheet.sort((1, 'asc'), (2, 'asc'));
-		search_query = input("INPUT SEARCH WORDS");
-		search_genre = input("INPUT SEARCH GENRE\n" + GENRE_TXT);
-		search_sort = input("INPUT IN WHAT WAY SORT\n" + SORT_TXT);
-		params = URLBuilder.Chiebukuro_Params(p=search_query, dnum=search_genre, b="1", sort=search_sort);
+		search_query = input("INPUT SEARCH WORDS" + "\n");
+		search_genre = input("INPUT SEARCH GENRE" + "\n" + GENRE_TXT);
+		search_sort = input("INPUT IN WHAT WAY SORT" + "\n" + SORT_TXT);
+		params = URLBuilder.Chiebukuro_Params(dict(p=search_query, dnum=search_genre, b="1", sort=search_sort));
 		print("if you want to get out of the process, press [Ctrl+C].")
-		Scraper(search_query).scrape_execute(params, ws, url_keys, userid_keys, visited);
-
-
+		Scraper(search_query).scrape_execute(params, ws, url_keys, userid_keys, visited, dbg=0);
 
 if __name__ == "__main__":
 	main();
